6096dd8acc50ccec01694d6156e82821feda402b
NoorAli <alitalisvi@gmail>
2022-09-12 21:30:56 +0500

    change the app.js file developer name is added

--- fyp-frontend/src/App.js
+++ fyp-frontend/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcom to Old Book Shop</h1>
+        <p>Developed by Noor Ali</p>
       </header>
     </div>
   );
